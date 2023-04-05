748d67df4db33e38bd3eaff82a599493b6a7f808
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-04-05 22:10:57 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,12 +6,5 @@ chat_id = 1188007817 # Ваш chat ID, не меняйте название пе
 
 def solution(x: np.array) -> float:
     n = len(x)
-    alpha = 1 - p
-    chi_sq = np.sum(x**2, axis=1) / (37 * np.var(x))
-    chi_left = chi2.ppf(q=alpha/2, df=1)
-    chi_right = chi2.ppf(q=1-alpha/2, df=1)
-    sigma_left = np.sqrt(np.sum(x**2) / (37 * chi_right))
-    sigma_right = np.sqrt(np.sum(x**2) / (37 * chi_left))
-    list1 = [sigma_left, sigma_right]
-    return list1
-
+    total = sum(x)
+    return total / (n*6) # Ваш ответ
